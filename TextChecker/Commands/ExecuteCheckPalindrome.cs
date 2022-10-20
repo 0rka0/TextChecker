307bab3720c27c338eb96139f98cd39a01b9b724
David Friedwagner <david.fw3004@gmail.com>
2022-10-20 14:44:50 +0200

    Viewmodel, Command added and connected to UI

---
+++ TextChecker/Commands/ExecuteCheckPalindrome.cs
@@ -0,0 +1,46 @@
+﻿namespace TextChecker.Commands
+{
+    using System;
+    using System.Windows.Input;
+    using TextChecker.Viewmodels;
+
+    class ExecuteCheckPalindrome : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public ExecuteCheckPalindrome(MainViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+            this.viewModel.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "TextInput")
+                {
+                    CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+                }
+            };
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return !string.IsNullOrWhiteSpace(viewModel.TextInput);
+        }
+
+        public void Execute(object? parameter)
+        {
+            bool result = true; //Check for palindrome in BL
+
+            if (result)
+            {
+                viewModel.ResultOutput = $"{viewModel.TextInput} ist ein Palindrom.";
+            }
+            else
+            {
+                viewModel.ResultOutput = $"{viewModel.TextInput} ist kein Palindrom.";
+            }
+
+            viewModel.TextInput = string.Empty;
+        }
+
+        private readonly MainViewModel viewModel;
+    }
+}
---
+++ TextChecker/Viewmodels/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿namespace TextChecker.Viewmodels
+{
+    using System.Windows.Input;
+    using TextChecker.Commands;
+
+    class MainViewModel : ViewModelBase
+    {
+        public string TextInput
+        {
+            get
+            {
+                return textInput;
+            }
+            set
+            {
+                if (TextInput != value)
+                {
+                    textInput = value;
+                    OnPropertyChanged(nameof(TextInput));
+                }
+            }
+        }
+
+        public string ResultOutput
+        {
+            get
+            {
+                return resultOutput;
+            }
+            set
+            {
+                if (TextInput != value)
+                {
+                    resultOutput = value;
+                    OnPropertyChanged(nameof(TextInput));
+                }
+            }
+        }
+
+        public ICommand ExecuteCheckPalindrome { get; }
+
+        public MainViewModel()
+        {
+            this.ExecuteCheckPalindrome = new ExecuteCheckPalindrome(this);
+        }
+
+        private string textInput;
+
+        private string resultOutput;
+    }
+}
